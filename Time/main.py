387cfeb1a46db7974bc8fca5a7c915370f410705
Wb2D <ya.daniil2002.ru@ya.ru>
2023-11-17 02:48:47 +0300

    получил графическое представление зависимости числа траекторий от времени для разных методов

---
+++ Time/main.py
@@ -0,0 +1,18 @@
+import json
+import matplotlib.pyplot as plt
+
+json_file_path = "C:\Qt\Projects\CIR\Data\Plots\TeylorMethod\\time.json"
+
+with open(json_file_path, 'r') as file:
+    data = json.load(file)
+    results = data.get("results", [])
+
+iterations = [result["numberOfIterations"] for result in results]
+elapsed_times = [result["elapsedTime"] for result in results]
+
+plt.plot(iterations, elapsed_times, marker='o', linestyle='-')
+plt.title('Dependence of time on the number of trajectories')
+plt.xlabel('m, trajectories')
+plt.ylabel('t, time')
+plt.grid(True)
+plt.show()
