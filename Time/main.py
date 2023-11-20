219ec473b68e4edfed751369462fd0fc4f29797c
Wb2D <ya.daniil2002.ru@ya.ru>
2023-11-20 14:58:29 +0300

    внес правки в анализ зависимости времени от числа траекторий

--- Time/main.py
+++ Time/main.py
@@ -1,7 +1,8 @@
 import json
 import matplotlib.pyplot as plt
+import numpy as np
 
-json_file_path = "C:\Qt\Projects\CIR\Data\Plots\RK3\\time.json"
+json_file_path = r"C:\Qt\Projects\CIR\Data\Plots\TeylorMethod\time.json"
 
 with open(json_file_path, 'r') as file:
     data = json.load(file)
@@ -10,9 +11,20 @@ with open(json_file_path, 'r') as file:
 iterations = [result["numberOfIterations"] for result in results]
 elapsed_times = [result["elapsedTime"] for result in results]
 
-plt.plot(iterations, elapsed_times, marker='o', linestyle='-')
+# линейная аппроксимация
+coefficients = np.polyfit(iterations, elapsed_times, 1)
+polynomial = np.poly1d(coefficients)
+
+# угловой коэффициент
+k = coefficients[0]
+print(f"Угловой коэффициент (k): {k}")
+
+plt.plot(iterations, polynomial(iterations), linestyle='-', label=f'Trendline (k={k:.4f})', zorder=99)
+plt.plot(iterations, elapsed_times, marker='o', markersize=5, linestyle='-', label='Actual Data')
+
 plt.title('Dependence of time on the number of trajectories')
 plt.xlabel('m, trajectories')
 plt.ylabel('t, time')
 plt.grid(True)
+plt.legend()
 plt.show()
